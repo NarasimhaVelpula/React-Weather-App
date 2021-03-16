2262d3b91c244c5ac1ef4e196a4de29ef2aad7e3
Narasimha <naninarasimha@gmail.com>
2021-03-16 16:07:36 +0530

    initial-release

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from 'react'
+import Wheather from './Components/Wheather';
+const Api_key="ccd28502fe5a1a103df9547238494117"
 function App() {
+  const [state, setState] = useState({"coord":{"lon":81.7833,"lat":16.9833},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50d"}],"base":"stations","main":{"temp":304.15,"feels_like":307.64,"temp_min":304.15,"temp_max":304.15,"pressure":1012,"humidity":58},"visibility":3000,"wind":{"speed":1.54,"deg":30},"clouds":{"all":20},"dt":1615880885,"sys":{"type":1,"id":9225,"country":"IN","sunrise":1615855215,"sunset":1615898575},"timezone":19800,"id":1258932,"name":"Rajahmundry","cod":200})
+  const [city,setCity]=useState("Hyderabad")
+  useEffect(()=>{
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api_key}`)
+    .then(response=>response.json())
+    .then(response=>{setState(response)
+    console.log(response)
+    })
+    
+    
+  },[city])
+  const changeCity=e=>{
+    e.preventDefault()
+   // console.log(e.target[0].value)
+    setCity(e.target[0].value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={changeCity}>
+          Enter City Name<input type="text" />
+          <button type="submit">Enter</button>
+
+      </form>
+      <Wheather tempDetails={state}/>
     </div>
   );
 }
---
+++ src/Components/Wheather.js
@@ -0,0 +1,78 @@
+import React from 'react'
+
+function Wheather(props) {
+    console.log(props)
+    if(props.tempDetails.cod==="404"){
+        console.log("enter here")
+        return(
+            <h1>City Not found</h1>
+        )
+    }
+    let weatherClass=""
+    function findIcon(id,day){
+        if(id>=200 && id<=232){
+            weatherClass="thunderstorm"
+        }
+        else if(id>=300 && id<=321){
+            weatherClass="rain-mix"
+        }
+        else if(id>=500 && id<=531){
+            weatherClass="rain"
+        }
+        else if(id>=600 && id<=622){
+            weatherClass="snow"
+        }
+        else if(id>=701 && id<=781){
+            weatherClass="haze"
+        }
+        else if(id>=801 && id<=804){
+            weatherClass="cloudy"
+        }
+        else{
+            if(day[day.length-1]==="d"){
+            weatherClass="sunny"
+            }
+            else{
+                weatherClass="clear"
+            }
+        }
+    }
+    let dayNight=""
+    function findDayNight(id){
+        if(id[id.length-1]==="d"){
+            dayNight="day"
+        }
+        else{
+            dayNight="night"
+        }
+    }
+    const cityName=props.tempDetails.name;
+    const temperature=parseInt(props.tempDetails.main.temp-273.15)
+    const minTemp=parseInt(props.tempDetails.main.temp_min-273.15);
+    const maxTemp=parseInt(props.tempDetails.main.temp_max-273.15);
+    const description=props.tempDetails.weather[0].description
+    findIcon(props.tempDetails.weather[0].id,props.tempDetails.weather[0].icon)
+    findDayNight(props.tempDetails.weather[0].icon)
+    weatherClass="wi wi-"+dayNight+"-"+weatherClass+" display-1"
+    return (
+        <div>
+            <h1 className="py-2">{cityName}</h1>
+            <h5>
+            <i className={weatherClass}></i>
+            </h5>
+            <h1 className="py-2">{temperature}&deg;</h1>
+            {minmaxTemp(minTemp,maxTemp)}
+            <h4 className="py-4">{description}</h4>
+        </div>
+    )
+}
+function minmaxTemp(min,max){
+    return(
+       <h3>
+           <span className="px-4">{min}&deg;</span>
+           <span className="px-4">{max}&deg;</span>
+       </h3>
+    )
+}
+
+export default Wheather
