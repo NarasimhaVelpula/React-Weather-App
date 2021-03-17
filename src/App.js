2428a5e6a7d11122908cf447945e1576caca836d
Narasimha <naninarasimha@gmail.com>
2021-03-17 14:12:10 +0530

    initial-release added https

--- src/App.js
+++ src/App.js
@@ -7,13 +7,11 @@ function App() {
   const [state, setState] = useState({"coord":{"lon":81.7833,"lat":16.9833},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50d"}],"base":"stations","main":{"temp":304.15,"feels_like":307.64,"temp_min":304.15,"temp_max":304.15,"pressure":1012,"humidity":58},"visibility":3000,"wind":{"speed":1.54,"deg":30},"clouds":{"all":20},"dt":1615880885,"sys":{"type":1,"id":9225,"country":"IN","sunrise":1615855215,"sunset":1615898575},"timezone":19800,"id":1258932,"name":"Rajahmundry","cod":200})
   const [city,setCity]=useState("Hyderabad")
   useEffect(()=>{
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api_key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api_key}`)
     .then(response=>response.json())
     .then(response=>{setState(response)
     console.log(response)
     })
-    
-    
   },[city])
   const changeCity=e=>{
     e.preventDefault()
